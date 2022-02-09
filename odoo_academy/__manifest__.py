6e3334a320500f1b9f5e51a7b87b288b5c87bf9d
robhparker <odoo@ampd1.odoo.com>
2022-02-09 03:05:50 +0000

    [ADD] Basic Module Testing

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*- 
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """Academy app to manage Training Stuff""",
+    
+    'description': """
+        Academy Module to manage Training:
+        -Courses
+        -Sessions
+        -Attendees
+    """,
+    
+    'author': 'PHS',
+    
+    'website': 'https://www.parkhealthsystems.com',
+    
+    'category': 'Training',
+    'version':  '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+        
+    ],
+    
+    'demo': [
+        
+        
+    ],
+
+}
+
+
+
